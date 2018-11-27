6750b27f2fb1422992bd980b2ed0ed64e69b8ddd
wangqiangqiang <764372409@qq.com>
2018-11-27 17:35:05 +0800

    更改数据库路径

--- src/main/java/com/yuanmaxinxi/RepaymentSystemApplication.java
+++ src/main/java/com/yuanmaxinxi/RepaymentSystemApplication.java
@@ -4,10 +4,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
+import com.yuanmaxinxi.util.DBUtil;
+
+
 @SpringBootApplication
 @EnableScheduling
 public class RepaymentSystemApplication {
 	public static void main(String[] args) {
+		DBUtil.createTableBaseData();
 		SpringApplication.run(RepaymentSystemApplication.class, args);
 	}
 }
